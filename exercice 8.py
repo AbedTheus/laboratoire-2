063ee33a12853e48bd36e9f6b67c4bd2591067da
Abed715 <206304632@cegepsherbrooke.qc.ca>
2025-01-29 15:19:56 -0500

    Update exercice 8.py

---
+++ exercice 8.py
@@ -0,0 +1,9 @@
+def addition(a, b):
+    resultat = a + b
+    return resultat
+#après le copier coler le tab s'enleve, il faut faire attention
+x = 5
+#enlever les "" car le 5 etais ecrit en str
+y = 3
+z = addition(x, y)
+print("Résultat:", z)

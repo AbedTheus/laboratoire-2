16703014750b7b34353ae404afd71f2754392214
Abed715 <206304632@cegepsherbrooke.qc.ca>
2025-01-29 14:40:02 -0500

    Update exercice 5.py

---
+++ exercice 5.py
@@ -0,0 +1,5 @@
+"""poser la question as tu finis le programme
+si oui fin du programme
+si non  est ce qu'il fait moins chaud que 20 degrées
+ si oui allumer le chauffage et apres avoir allumé le chauffage, ca te ramene a l'étape 1
+ si non, te rammene a l'etape 1"""
